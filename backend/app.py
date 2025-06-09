bb8e0275bdac86928687f75a77ef274b9f690568
Siva Sandeep S <sandeepsivakanthan@gmail.com>
2025-06-09 12:15:54 +0530

    Update app.py to work with Render deployment

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,16 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
 import sqlite3
+import os
+
 
 app = Flask(__name__)
 CORS(app)  # Allow React frontend to fetch data
 
-@app.route('/api/data')
-def get_data():
-    conn = sqlite3.connect('my_database.db')
-    cur = conn.cursor()
-    cur.execute("SELECT time, weight, status FROM weight_data ORDER BY time ASC")
-    rows = cur.fetchall()
-    conn.close()
-    return jsonify([{"time": r[0], "weight": r[1], "status": r[2]} for r in rows])
+@app.route('/')
+def home():
+    return 'Backend is working!'
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
